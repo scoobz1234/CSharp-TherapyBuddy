fcfb8e9f36e8dbe740980b94fb23e9ef3573a138
STEPHEN OUELLETTE <STEOUELL@uat.edu>
2019-07-20 11:26:34 -0700

    Project Creation

---
+++ TherapyBuddy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TherapyBuddy
+{
+    static class Program
+    {
+        /// <summary>
+        /// Therapy Buddy is a program created by UAT students to aid Therapists in the process
+        /// of ensuring propor documentation is performed.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
